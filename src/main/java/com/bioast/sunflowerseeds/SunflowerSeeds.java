a178cb76909c6e16ebdb426d13608fbb5b309b05
Bioastroiner <naderi81eco@gmail.com>
2020-11-10 05:34:28 +0330

    prep

---
+++ src/main/java/com/bioast/sunflowerseeds/SunflowerSeeds.java
@@ -0,0 +1,42 @@
+package com.bioast.sunflowerseeds;
+
+import com.bioast.sunflowerseeds.init.ItemsMod;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+import static com.bioast.sunflowerseeds.utils.refs.ModId.modid;
+
+@Mod(modid)
+public class SunflowerSeeds
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public SunflowerSeeds() {
+        final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
+        bus.addListener(this::doClientStuff);
+        register(bus);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void register(IEventBus bus){
+        ItemsMod.itemRegister.register(bus);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+}
